1d15fca651b4b3375da240ef2763e3f5cba5d711
Desperta Dev <devdesperta@gmail.com>
2026-01-01 16:36:52 -0300

    Atualizando Lista de Exercicios

--- src/br/despertadev/estruturasequencial/NomeString.java
+++ src/br/despertadev/estruturasequencial/NomeString.java
@@ -2,20 +2,21 @@
  * em Java com Orientação a Objetos e Padrão de Projeto
  * Autor: DESPERTA DEV
  * Data: 01/01/2026
- * Exercicio 3 -  Faça um programa em Linguagem Java, para ler um número real e mostrar ele na tela.
+ * Exercicio Faça um programa em Linguagem Java, para ler um nome e mostrar ele na tela.
  * */
 
 package br.despertadev.estruturasequencial;
 
 import java.util.Scanner;
 
-public class NumeroReal {
+public class NomeString {
+
     static void main() {
         //Cria um objeto sc da classe Scanner para fazer a leitura de dados do Teclado
         Scanner sc = new Scanner(System.in);
-        System.out.println("Entre com um número Real");
-        double real = sc.nextDouble(); //Em Java os números reais ou fracionarios são anotados como double
-        System.out.println("O número digitado foi: " + real);
+        System.out.println("Entre com um nome:");
+        String nome = sc.next();
+        System.out.println("O nome digitado foi: " + nome);
         sc.close(); // Encerra o Objeto sc da classe Scanner
     }
 }
